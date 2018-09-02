255f632b60aa9f30b7a0633ec0459253a83722fd
Akash <janardhangrd@gmail.com>
2018-09-02 14:33:42 +0530

    1stcommit

---
+++ WebAppForGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppForGit.Startup))]
+namespace WebAppForGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
